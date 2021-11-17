5c3a61092426c611b3322fe80c13bac1134b166e
brnt-toast <69434107+brnt-toast@users.noreply.github.com>
2021-11-17 09:36:57 -0500

    fix: typo

--- python/writef.py
+++ python/writef.py
@@ -1,4 +1,4 @@
-def writef(file_name,extention,content):
-    with open(f"{file_name}.{extention}",'w',encoding='utf-8') as f:
+def writef(file_name,extension,content):
+    with open(f"{file_name}.{extension}",'w',encoding='utf-8') as f:
         f.write(str(content))
         f.close()
