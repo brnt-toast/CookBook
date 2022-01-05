96fdde2b401d75a8a089f96687067930d9ad5cc5
brnt-toast <brnt-toast@gmail.com>
2022-01-05 17:24:59 -0500

    test

---
+++ dart/vars.dart
@@ -0,0 +1,13 @@
+// String
+var str = 'hello world';
+// Integer 
+var number = 1776;
+// Float
+var float_number = 3.14;
+// Vector
+var list = ['a','b','c','x','y','z'];
+// Hash Table
+var object = {
+	'colors' : ['red','green','blue'],
+	'hello' : 'world'
+};
