2e3a9b7948be1800bd9c79d3d43f9a1c6bdde52c
brnt-toast <yoyo.beerme@gmail.com>
2022-04-21 11:44:47 -0400

    init

---
+++ languages/javascript/react/react-router-dom/Link.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Routes, Route, Link} from 'react-router-dom';
+
+import Home from './pages/Home'
+
+export default App(){
+	return (
+		<>
+			<h1> Hello from App.js </h1>
+			<Link exact to="/"> Home </Link>
+
+			<hr />
+
+			<Routes>
+				<Route path="/" element={<Home />} />	
+			<Routes>
+		</>
+	)
+}
