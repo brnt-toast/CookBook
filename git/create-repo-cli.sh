f4d3b590998d5b45357ffde7206e7e9ec2a9835d
brnt-toast <yoyo.beerme@gmail.com>
2023-02-21 17:33:50 -0500

    init create-repo.cli.sh

---
+++ git/create-repo-cli.sh
@@ -0,0 +1,4 @@
+# https://www.shellhacks.com/create-gitlab-project-repository-from-command-line/
+
+git push --set-upstream https://gitlab.com/<username>/<projectName>.git
+git remote add origin https://gitlab.com/<username>/<projectName>.git
