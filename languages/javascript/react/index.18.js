de48a65dfae265a1701bbcb1fbb67989f6d05380
brnt-toast <69434107+brnt-toast@users.noreply.github.com>
2023-03-09 13:21:33 -0500

    bugfix: typo on line 6; react import line 4

--- languages/javascript/react/index.18.js
+++ languages/javascript/react/index.18.js
@@ -1,9 +1,9 @@
 // ...Upgrade to React 18
 // https://reactjs.org/blog/2022/03/08/react-18-upgrade-guide.html
 // --
-
+import React from 'react';
 import {createRoot} from 'react-dom/client';
-import from App from './App';
+import App from './App';
 
 const root = createRoot(
 	document.querySelector('#app')
