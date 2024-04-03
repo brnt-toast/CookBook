f27e4963b3551c6aacd733b5d194a9e52d4b8049
brnt-toast <yoyo.beerme@gmail.com>
2024-04-03 18:29:42 -0400

    ಠ_ಠ

--- langs/python/OOP/classes.py
+++ langs/python/OOP/classes.py
@@ -1,7 +1,12 @@
 class Car: 
     def __init__(self, brand, model):
         self.brand = brand
-        self.brand = model
+        self.model= model
 
     def drive(self):
             return f'{self.brand} {self.model} is driving'
+
+car_1 = Car("Toyota", "Camry")
+
+print(car_1.brand)
+print(car_1.model)
