c26a52a890cf9e94d3a5c121fa4c8edee88d50fb
brnt-toast <yoyo.beerme@gmail.com>
2024-04-03 18:26:35 -0400

    ε=ε=ε=┌(;*´Д`)ﾉ

---
+++ langs/python/OOP/classes.py
@@ -0,0 +1,7 @@
+class Car: 
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.brand = model
+
+    def drive(self):
+            return f'{self.brand} {self.model} is driving'
