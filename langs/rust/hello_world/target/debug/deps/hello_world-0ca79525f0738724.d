0bff926870b85b0c7a87f32368e327b906849eb0
brnt-toast <yoyo.beerme@gmail.com>
2024-04-04 04:59:13 -0400

    hello world

---
+++ langs/rust/hello_world/target/debug/deps/hello_world-0ca79525f0738724.d
@@ -0,0 +1,5 @@
+/home/blacc/CookBook/langs/rust/hello_world/target/debug/deps/hello_world-0ca79525f0738724: src/main.rs
+
+/home/blacc/CookBook/langs/rust/hello_world/target/debug/deps/hello_world-0ca79525f0738724.d: src/main.rs
+
+src/main.rs:
