ffdef21e7bddaf39aa978c52c1ec01505201091c
brnt-toast <yoyo.beerme@gmail.com>
2024-04-07 17:25:49 -0400

    щ(ﾟДﾟщ)

--- langs/rust/guessing_game/src/main.rs
+++ langs/rust/guessing_game/src/main.rs
@@ -1,5 +1,6 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number!");
@@ -15,6 +16,14 @@ fn main() {
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
+    
+    let guess: u32 = guess.trim().parse().expect("Not a Number");
 
     println!("You guessed: {guess}");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small"),
+        Ordering::Greater => println!("Too big"),
+        Ordering::Equal => println!("Winner"),
+    }
 }
