5fa84987901c390d244d70a3b0b9e290bc6f1466
brnt-toast <yoyo.beerme@gmail.com>
2024-05-27 11:37:26 -0400

    }~<(((*>

--- data-structures/min_heap.py
+++ data-structures/min_heap.py
@@ -22,12 +22,46 @@ class MinHeap:
         left = self._left_child(index)
         right = self._right_child(index)
 
-    def insert():
-        pass
-    def extract_min():
-        pass
-    def peek():
-        pass
-    def __str__():
-        pass
+        if left < len(self.heap) and self.heap[left] < self.heap[smallest]:
+            smallest = left
+        if right < len(self.heap) and self.heap[right] < self.heap[smallest]:
+            smallest = right
+        
+        if smallest != index:
+            self.heap[index], self.heap[smallest] = self.heap[smallest], self.heap[index]
+            self._heapify_down(smallest)
 
+    def insert(self, element):
+        self.heap.append(element)
+        self._heapify_up(len(self.heap) - 1)
+
+    def extract_min(self):
+        if len(self.heap) == 0:
+            raise IndexError("extract_min from empty heap")
+        if len(self.heap) == 1:
+            return self.heap.pop()
+
+        root = self.heap[0]
+        self.heap[0] = self.heap.pop()
+        self._heapify_down(0)
+        return root
+
+    def peek(self):
+        if len(self.heap) == 0:
+            raise IndexError("peek from empty heap")
+        return self.heap[0]
+
+    def __str__(self):
+        return str(self.heap)
+
+min_heap = MinHeap()
+min_heap.insert(10)
+min_heap.insert(5)
+min_heap.insert(3)
+min_heap.insert(2)
+min_heap.insert(8)
+
+print("Min-Heap:", min_heap)
+print("Extract Min:", min_heap.extract_min())
+print("Min-Heap after extraction:", min_heap)
+print("Peek:", min_heap.peek())
